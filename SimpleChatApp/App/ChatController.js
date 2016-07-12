d5cc7168cfe947f2c7880edf045f983f56ca67a7
linckon <linckon@live.com>
2016-07-12 23:37:48 +0600

    added comment in chatcontroller.ts

--- SimpleChatApp/App/ChatController.js
+++ SimpleChatApp/App/ChatController.js
@@ -9,20 +9,22 @@ var App;
     var ChatController = (function () {
         function ChatController(rootScope) {
             var _this = this;
+            // collection of messages coming from server
             this.messages = [];
             this.chatModel = new Communication();
             this.rootScope = rootScope;
             this.connection = $.hubConnection();
-            this.chatHub = this.connection.createHubProxy('ChatHub');
+            this.chatHub = this.connection.createHubProxy('ChatHub'); // initializes hub
             this.connection.start();
             // register a client method on hub to be invoked by the server
             this.chatHub.on('broadcastMessage', function (name, message) {
                 var newMessage = name + ' says: ' + message;
-                _this.messages.push(newMessage);
+                _this.messages.push(newMessage); // push the newly coming message to the collection of messages
                 _this.rootScope.$apply();
             });
         }
         ChatController.prototype.send = function () {
+            // sends a new message to the server
             this.chatHub.invoke('send', this.chatModel.name, this.chatModel.message);
             this.chatModel.message = '';
         };
--- SimpleChatApp/App/ChatController.ts
+++ SimpleChatApp/App/ChatController.ts
@@ -8,21 +8,22 @@
         messages: string[];
         chatModel: Communication;
         private connection: HubConnection;
-        private chatHub: HubProxy;
+        private chatHub: HubProxy;// holds the reference to hub
         rootScope:angular.IRootScopeService;
         static $inject: string[] = ["$rootScope"];
         constructor(rootScope: angular.IRootScopeService) {
+        // collection of messages coming from server
             this.messages = [];
             this.chatModel = new Communication();
             this.rootScope = rootScope;
             this.connection = $.hubConnection();
-            this.chatHub = this.connection.createHubProxy('ChatHub');
+            this.chatHub = this.connection.createHubProxy('ChatHub');// initializes hub
             this.connection.start(); 
 
             // register a client method on hub to be invoked by the server
             this.chatHub.on('broadcastMessage', (name, message) => {
                 var newMessage = name + ' says: ' + message;
-                this.messages.push(newMessage);
+                this.messages.push(newMessage);// push the newly coming message to the collection of messages
                 this.rootScope.$apply();
             });    
         }
@@ -30,6 +31,7 @@
 
 
         private send(): void {
+         // sends a new message to the server
             this.chatHub.invoke('send', this.chatModel.name, this.chatModel.message);
             this.chatModel.message = '';
         }
